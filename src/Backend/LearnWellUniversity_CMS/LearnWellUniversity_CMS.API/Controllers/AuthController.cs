531afbe27372be08bdba623208bb96a1cf9585b6
Zunayed Shahriar <zunayedshahriar@gmail.com>
2026-03-28 14:11:14 +0600

    feat: initialize LearnWellUniversity CMS backend project structure
    - Add solution file and project files for API, Application, Domain, Infrastructure, and Shared layers
    - Implement basic authentication endpoint with placeholder logic
    - Configure Docker support, and Swagger documentation with UI

---
+++ src/Backend/LearnWellUniversity_CMS/LearnWellUniversity_CMS.API/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using LearnWellUniversity_CMS.Application.DTOs.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnWellUniversity_CMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Authenticate([FromBody] AuthenticationRequest request)
+        {
+            return Ok(new { Message = "Authentication Successful." });
+        }
+    }
+}
---
+++ src/Backend/LearnWellUniversity_CMS/LearnWellUniversity_CMS.API/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Learn Well University - Course Management System", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
